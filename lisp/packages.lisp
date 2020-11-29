2d1f1d0b1de1b4971d7212682d2f6a453fc53bc2
Tarn W. Burton <twburton@gmail.com>
2020-11-29 10:45:42 -0500

    Add full screen button

--- lisp/packages.lisp
+++ lisp/packages.lisp
@@ -1,6 +1,9 @@
 (defpackage #:resizable-box
   (:use #:common-lisp)
   (:export
+    #:enable-full-screen
+    #:enter-full-screen
+    #:exit-full-screen
     #:full-screen
     #:resizable-grid-box
     #:resizable-layout
